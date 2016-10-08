b4139f733bc40405dc02895df58e4aa61a070176
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2016-10-08 02:47:36 +0200

    add track model

--- track_model/track_model_sigmoid.py
+++ track_model/track_model_sigmoid.py
@@ -204,9 +204,9 @@ def generate_model(split, config):
 
     # Dynamic conv filters
     # L2 Normalize image and language features
-    n.dyn_l, n.dyn_relu = fc_relu(n.lstm_feat, 1000+8)
-    n.dyn_relu_l2norm = L.L2Normalize(n.dyn_relu)
-    n.dyn_relu_l2norm_resh = L.Reshape(n.dyn_relu_l2norm,
+    n.dyn_l, n.dyn_sig = fc_sigmoid(n.lstm_feat, 1000+8)
+    n.dyn_sig_l2norm = L.L2Normalize(n.dyn_sig)
+    n.dyn_sig_l2norm_resh = L.Reshape(n.dyn_sig_l2norm,
                                      reshape_param=dict(shape=dict(dim=[-1, config.lstm_dim+8])))
 
     # Concatenate
@@ -222,7 +222,7 @@ def generate_model(split, config):
     #    n.scores = fc(n.mlp_drop1, 1)
     #else:
     #    n.scores = fc(n.mlp_relu1, 1)
-    n.scores = L.DotProductSimilarity(n.dyn_relu_l2norm_resh, n.feat_all_l2norm_resh)
+    n.scores = L.DotProductSimilarity(n.dyn_sig_l2norm_resh, n.feat_all_l2norm_resh)
 
     # Loss Layer
     # n.loss = L.ContrastiveLoss(n.dyn_sig1, n.feat_all, n.label)
@@ -342,9 +342,9 @@ def generate_scores(split, config):
 
     # Dynamic conv filters
     # L2 Normalize image and language features
-    n.dyn_l, n.dyn_relu = fc_relu(n.lstm_feat, 1000+8)
-    n.dyn_relu_l2norm = L.L2Normalize(n.dyn_relu)
-    n.dyn_relu_l2norm_resh = L.Reshape(n.dyn_relu_l2norm,
+    n.dyn_l, n.dyn_sig = fc_sigmoid(n.lstm_feat, 1000+8)
+    n.dyn_sig_l2norm = L.L2Normalize(n.dyn_sig)
+    n.dyn_sig_l2norm_resh = L.Reshape(n.dyn_sig_l2norm,
                                      reshape_param=dict(shape=dict(dim=[-1, config.lstm_dim+8])))
 
     # Concatenate
@@ -360,7 +360,7 @@ def generate_scores(split, config):
     #    n.scores = fc(n.mlp_drop1, 1)
     #else:
     #    n.scores = fc(n.mlp_relu1, 1)
-    n.scores = L.DotProductSimilarity(n.dyn_relu_l2norm_resh, n.feat_all_l2norm_resh)
+    n.scores = L.DotProductSimilarity(n.dyn_sig_l2norm_resh, n.feat_all_l2norm_resh)
 
     # Loss Layer
     # n.loss = L.ContrastiveLoss(n.dyn_sig1, n.feat_all, n.label)
