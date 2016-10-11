57a8baf971aded1cbf45568bc374654ff9b51a36
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2016-10-11 14:38:00 +0200

    add track model

--- track_model_dyn/referit_data_provider.py
+++ track_model_dyn/referit_data_provider.py
@@ -46,7 +46,7 @@ def run_prefetch(prefetch_queue, folder_name, prefix, num_batch, shuffle):
         # process the batch
         text_seq_val = batch['text_seq_batch']
         cont_val = create_cont(text_seq_val)
-        imcrop_val = batch['imcrop_batch'].astype(np.float32) - track_model.channel_mean
+        imcrop_val = batch['imcrop_batch'].astype(np.float32) - track_model_dyn_relu.channel_mean
         imcrop_val = imcrop_val.transpose((0, 3, 1, 2))
         spatial_batch_val = batch['spatial_batch']
         label_val = batch['label_batch'].astype(np.float32)
