133c2a081b5cb85e9e887825ada21e3695d28189
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2016-10-13 19:45:49 +0200

    add track model

--- lang_seg_low_res_model/lang_seg_low_res_model.py
+++ lang_seg_low_res_model/lang_seg_low_res_model.py
@@ -162,8 +162,8 @@ def generate_model(split, config):
     n.lstm_feat = L.Reshape(n.lstm_out, reshape_param=dict(shape=dict(dim=[-1, config.lstm_dim])))
 
     # Dynamic conv filters
-    n.dyn_l, n.dyn_relu = fc_sigmoid(n.lstm_feat, 1000+2)
-    n.lstm_dyn_kernel = L.Reshape(n.lstm_feat, reshape_param=dict(shape=dict(dim=[-1, 1, config.lstm_dim+2, 1, 1])))
+    n.dyn_l, n.dyn_relu = fc_sigmoid(n.lstm_feat, 1000+8)
+    n.lstm_dyn_kernel = L.Reshape(n.lstm_feat, reshape_param=dict(shape=dict(dim=[-1, 1, config.lstm_dim+8, 1, 1])))
 
     # Tile LSTM feature
     #n.lstm_resh = L.Reshape(n.lstm_feat, reshape_param=dict(shape=dict(dim=[-1, config.lstm_dim, 1, 1])))
