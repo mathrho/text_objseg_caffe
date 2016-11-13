479b0e9ede764cfe18577fd17655bbaffbe78a1c
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2016-11-13 22:37:50 +0100

    add ILSVRC code

--- demo/eval_bbox_ILSVRC.m
+++ demo/eval_bbox_ILSVRC.m
@@ -93,7 +93,7 @@ for vi = 1:numel(videos)
             oas(1, frame_counter) = max_ov;
         end
 
-        oas = oas(1, sum(gt_bboxes(:, 1:4), 2)>0);
+        oas = oas(sum(gt_bboxes(:, 1:4), 2)>0);
 
         %predBBoxFile = ['/home/zhenyang/Workspace/devel/project/vision/text_objseg_caffe/results/ILSVRC/' signiture '/' video '_query_' num2str(qi) '_prediction_rect.txt'];
         %dlmwrite(predBBoxFile, pred_boxes);
